8d7b4e5f5635bb96261ad213c18d35a4530da887
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-05-13 15:06:11 +0000

    Lösungsbeispiel CodeChallenge II

--- src/07_codechallenge_II.js
+++ src/07_codechallenge_II.js
@@ -19,17 +19,53 @@ Alter 13-17 // Cola
 
 /******* Vorüberlegung / SWITCH|CASE 2 *******/
 
-const cond = true;
-const value = 10;
+// const cond = true;
+// const value = 10;
 
-switch (cond) {
-    case (value == 1):
-        console.log("Value hat den Wert 1.");
-        break;
-    case (value == 2):
-        console.log("Value hat den Wert 2.");
-        break;
-    default:
-        console.log("Value hat einen anderen Wert.");
-        break;
+// switch (cond) {
+//     case (value == 1):
+//         console.log("Value hat den Wert 1.");
+//         break;
+//     case (value == 2):
+//         console.log("Value hat den Wert 2.");
+//         break;
+//     default:
+//         console.log("Value hat einen anderen Wert.");
+//         break;
+// }
+
+const prompt = require('prompt-sync')({sigint: true});
+const firstName = prompt("Bitte Namen eingeben: ");
+const age =  parseInt(prompt("Bitte Alter eingeben: ")); // "2" --> 2
+
+mapBeveragesToAge(firstName,age);
+
+
+function mapBeveragesToAge(name,age) {
+
+    switch (true) {
+        case (age >= 0) && (age <= 5) :
+            output(firstName + " trinkt Milch.");
+            break;
+        case (age >= 6) && (age <= 12):
+            output(firstName + " trinkt Saft.");
+            break;
+        case (age >= 13) && (age <= 17):
+            output(firstName + " trinkt Cola.");
+            break;
+        case (age >= 18) && (age <= 130):
+            output(firstName + " trinkt Wein.");
+            break;   
+        default:
+            output("Bitte Tee trinken!");   
+            break;
+    }
+    
 }
+
+function output(outputData) {
+    console.log(outputData);
+ }
+
+
+
