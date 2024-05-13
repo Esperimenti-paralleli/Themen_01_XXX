6316740bd90a2e7f263bedf0e8877f70702d9fcc
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-05-13 08:35:22 +0000

    Funktionen 01

---
+++ src/08_funktionen_I.js
@@ -0,0 +1,16 @@
+
+/***** Funktionen 01 *****/
+// 1. Kapselung von Codeblöcken
+
+// Funktionsaufruf | call(er)
+// test();
+
+// Funktionsrumpf | call(ee)
+// Funktionsdeklaration
+function test() 
+{
+    console.log("Hallo, Esther!");  
+}
+
+
+
